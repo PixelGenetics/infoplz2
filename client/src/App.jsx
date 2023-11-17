d0c779b58033c5002ff6d33b74ed5d0e88a0a3f2
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-11-16 22:45:43 -0600

    Showing information on front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react';
 import './App.css'
+import axios from "axios";
 
 function App() {
 
+  const [inicio,setInicio] = useState([]);
+
+  useEffect(() =>{
+      axios.get('http://localhost:3000/api/employees').then(resp =>{
+    console.log("sadasd",resp.data);
+    setInicio(resp.data)
+  })},[])
+
   return (
+
     <>
-      
+      {
+        inicio.map(item => (
+          <div className='main'  key={item.id}>
+
+            <div className='nombre'>
+              <p className=''>Nombre: {item.nombre}</p>
+            </div>
+
+            <div className='apellido'>
+              <p className=''>Apellido: {item.lastname}</p>
+            </div>
+
+          </div>
+        ))
+      }
     </>
   )
 }
