8fe9d9c8ee849a588d74089ecf08575e10b28750
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-11-16 22:46:04 -0600

    Importing and using cors

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import todoRouter from './routes/todo.routes.js';
 import employeesRoutes from './routes/employes.routes.js'
 import indexRouter from './routes/todo.routes.js'
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(todoRouter);
 
