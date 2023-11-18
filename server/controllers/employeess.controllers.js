11bcdd9db338822d68e4fd7c4443d6d84154cc7a
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-11-17 20:29:15 -0600

    Creating trycatch for errors and new message for status

--- server/controllers/employeess.controllers.js
+++ server/controllers/employeess.controllers.js
@@ -1,21 +1,36 @@
 import connection from "../db.js";
 
-export const getEmployees = async(req,res) => {
-    const [rows] =  await connection.query('SELECT * FROM data1')
-    res.json(rows);
+export const getPublicaciones = async(req,res) => {
+    try {
+        const [rows] =  await connection.query('SELECT * FROM data1')
+        res.json(rows);
+    } catch (error) {
+        return res.status(500).json({
+            message:"Something went wrong"
+        })
+    }
 };
 
-export const getEmployee = async(req,res) => {
+export const getPublicacion = async(req,res) => {
+    try {
     const [rows] = await connection.query('SELECT * FROM data1 WHERE id = ?',[req.params.id])
     console.log(rows)
+
     if(rows.length <= 0) return res.status(404).json({
         message:"Employee not found"
     })
     res.send(rows[0])
+
+    } catch (error) {
+    return res.status(500).json({
+        message:"Something went wrong"
+    })
+    }
 }
 
-export const postEmployees = async (req,res) => {
-    const {nombre, lastname,phone,birth} = req.body;
+export const postPublicacion = async (req,res) => {
+    try {
+        const {nombre, lastname,phone,birth} = req.body;
     const [rows] = await connection.query('INSERT INTO data1 (nombre,lastname,phone,birth) VALUES (?,?,?,?)',[nombre,lastname,phone,birth])
     res.send({
         id: rows.insertId,
@@ -24,31 +39,51 @@ export const postEmployees = async (req,res) => {
         phone,
         birth
     });
+    } catch (error) {
+        return res.status(500).json({
+            message:"Something went wrong"
+        })
+    }
 };
 
-export const updateEmployees = async (req,res) => {
+export const updatePublicacion = async (req,res) => {
+    try {
     const {id} = req.params
     const {nombre,lastname} = req.body
-
+    
     const [result] = await connection.query('UPDATE data1 SET nombre = IFNULL(?, nombre), lastname = IFNULL(?, lastname) WHERE id = ?', [nombre,lastname,id])
     console.log(result)
-
+    
     if(result.affectedRows <= 0 )return res.status(400).json({
         message:"Nothing to update"
     })
-
+    
     const [rows] = await connection.query('SELECT * FROM data1 WHERE id = ?',[id])
-
+    
     res.json(rows[0])
+
+    } catch (error) {
+        return res.status(500).json({
+            message:"Something went wrong"
+        })
+    }
 };
 
-export const deleteEmployees = async(req,res) => {
+export const deletePublicacion = async(req,res) => {
+    try {
     const result = await connection.query('DELETE FROM data1 WHERE id = ?',[req.params.id])
     console.log(result);
 
     if(result.affectedRows <= 0) return res.status(400).json({
         message: "Employee not found"
-    })
-
-    res.sendStatus(204)
+    });else{
+        return res.status(200).json({
+            message:"Employee deleted"
+        })
+    }
+    } catch (error) {
+        return res.status(500).json({
+            message:"Something went wrong"
+        })
+    }
 };
