6844a9539e6946c0b3c24e5548094a176aa0260b
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-11-20 02:26:12 -0600

    Ereasing component useless information

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,15 @@
-import { useState,useEffect } from 'react';
 import './App.css'
-import axios from "axios";
 import Header from './components/header';
 import Body from './components/Content/Body';
 function App() {
 
-  const [inicio,setInicio] = useState([]);
-
-  useEffect(() =>{
-      axios.get('http://localhost:3000/api/employees').then(resp =>{
-    //console.log("sadasd",resp.data);
-    setInicio(resp.data)
-  })},[])
-
   return (
 
     <>
-
-    <Header />
-    <Body />
+    <div className='h-[100vh] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+      <Header />
+      <Body />
+    </div>
     </>
   )
 }
