9f055fa1ef516ca28327d4a6497b12442f0cc6c7
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-11-20 02:27:24 -0600

    Adding content_summary to the endpoints

--- server/controllers/employeess.controllers.js
+++ server/controllers/employeess.controllers.js
@@ -13,7 +13,7 @@ export const getPublicaciones = async(req,res) => {
 
 export const getPublicacion = async(req,res) => {
     try {
-    const [rows] = await connection.query('SELECT * FROM contenido WHERE content_id = ?',[req.params.content_id])
+    const [rows] = await connection.query('SELECT * FROM contenido WHERE content_id = ?',[req.params.id])
     console.log(rows)
 
     if(rows.length <= 0) return res.status(404).json({
@@ -30,11 +30,12 @@ export const getPublicacion = async(req,res) => {
 
 export const postPublicacion = async (req,res) => {
     try {
-        const {content_author, content_parrafo,content_date} = req.body;
-    const [rows] = await connection.query('INSERT INTO contenido (content_author,content_parrafo,content_date) VALUES (?,?,?)',[content_author,content_parrafo,content_date])
+        const {content_author,content_summary, content_parrafo,content_date} = req.body;
+    const [rows] = await connection.query('INSERT INTO contenido (content_author,content_summary, content_parrafo,content_date) VALUES (?,?,?,?)',[content_author,content_summary,content_parrafo,content_date])
     res.send({
         content_id: rows.insertId,
         content_author,
+        content_summary,
         content_parrafo,
         content_date
     });
@@ -47,17 +48,17 @@ export const postPublicacion = async (req,res) => {
 
 export const updatePublicacion = async (req,res) => {
     try {
-    const {id} = req.params
-    const {nombre,lastname} = req.body
+    const {id} = req.params 
+    const {content_author,content_summary,content_parrafo} = req.body
     
-    const [result] = await connection.query('UPDATE data1 SET nombre = IFNULL(?, nombre), lastname = IFNULL(?, lastname) WHERE id = ?', [nombre,lastname,id])
+    const [result] = await connection.query('UPDATE contenido SET content_author = IFNULL(?, content_author), content_summary = IFNULL(?, content_summary), content_parrafo = IFNULL(?, content_parrafo) WHERE content_id = ?', [content_author,content_summary,content_parrafo,id])
     console.log(result)
     
     if(result.affectedRows <= 0 )return res.status(400).json({
         message:"Nothing to update"
     })
     
-    const [rows] = await connection.query('SELECT * FROM data1 WHERE id = ?',[id])
+    const [rows] = await connection.query('SELECT * FROM contenido WHERE content_id = ?',[id])
     
     res.json(rows[0])
 
@@ -70,7 +71,7 @@ export const updatePublicacion = async (req,res) => {
 
 export const deletePublicacion = async(req,res) => {
     try {
-    const result = await connection.query('DELETE FROM data1 WHERE id = ?',[req.params.id])
+    const result = await connection.query('DELETE FROM contenido WHERE content_id = ?',[req.params.id])
     console.log(result);
 
     if(result.affectedRows <= 0) return res.status(400).json({
