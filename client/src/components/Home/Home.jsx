1fe48b3bfb4c0ef03f7b6b31954f1900649f8ab0
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-11-23 21:26:14 -0600

    Moving file

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from '../Header/Header.jsx'
+import Body from '../Content/Body.jsx'
+const Home = () =>{
+    return(
+        <>
+            <Header />
+            <Body /> 
+        </>
+    )
+}
+
+export default Home
