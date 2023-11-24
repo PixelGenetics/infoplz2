d98211440773c4003e7228f0a696844deac87b64
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-11-23 21:25:45 -0600

    Moving file

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () =>{
+    return(
+    <>
+    <div className='  '>
+
+    <div>
+        <ul className='flex flex-row gap-20 justify-center pt-5 pb-5'>
+            <button><li className='border-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-1 rounded-md shadow-xl'>Home</li></button>
+            <button><li className='border-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-1 rounded-md shadow-xl'>Articulos</li></button>
+            <button><li className='border-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-1 rounded-md shadow-xl'>Linkedin</li></button>
+        </ul>
+    </div>
+
+    </div>
+    </>
+    )
+}
+
+export default Header
