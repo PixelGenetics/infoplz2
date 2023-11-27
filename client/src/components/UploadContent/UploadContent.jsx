a14e7251b01bea9ffe57233b4054ce8131ba43a5
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-11-26 18:52:52 -0600

    Changing name to form

--- client/src/components/UploadContent/UploadContent.jsx
+++ client/src/components/UploadContent/UploadContent.jsx
@@ -2,7 +2,7 @@ const UploadContent = () =>{
     return(
         <>
             <form action="/profile" method="post" encType="multipart/form-data">
-                <input type="file" name="avatar"/>
+                <input type="file" name="imagen"/>
                 <button type="submit">Enviar</button>
             </form>
             <p>Hola mundo</p>
