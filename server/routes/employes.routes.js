ed5f859bdf1febb224fbe294fdefc25e7755fd45
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-11-26 18:54:46 -0600

    Creating route to post files, importing multer and "guardarImagen" module

--- server/routes/employes.routes.js
+++ server/routes/employes.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { deletePublicacion, getPublicaciones, postPublicacion, updatePublicacion, getPublicacion } 
 from "../controllers/publicar.controllers.js";
+import multer from "multer";
+import {guardarImagen} from "../controllers/storage.controller.js";
 // const uploadModule = require('./uploadModule');
-import {upload} from '../controllers/storage.controller.js'
 
 const router = Router();
 
@@ -12,7 +13,13 @@ router.get('/publicacion/:id', getPublicacion);
 
 router.post('/publicacion', postPublicacion);
 
-router.post('/upload', upload)
+const storage = multer.memoryStorage();
+const subir = multer({storage:storage});
+
+router.post('/upload', subir.single('imagen'),(req,res) => {
+    guardarImagen(req,res);
+})
+
 
 router.patch('/publicacion/:id', updatePublicacion);
 
