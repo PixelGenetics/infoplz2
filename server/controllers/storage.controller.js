800d78230193000cad0a8ead31da62909e51556f
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-11-28 19:25:45 -0600

    Creating logic to show/get image

--- server/controllers/storage.controller.js
+++ server/controllers/storage.controller.js
@@ -3,8 +3,8 @@ import connection from "../db.js";
 import sharp from "sharp";
 
     export function guardarImagen(req,res) {
-    const nombre = req.file.originalname;
-    const datosImagen = req.file.buffer;
+    const nombre = req.file.originalname; //req.file.originalname => originalname proviene de multer
+    const datosImagen = req.file.buffer; //req.file.buffer => buffer proviene de multer
 
     const sql = 'INSERT INTO contenido_photo (nombre,datosImagen) VALUES (?,?)';
     connection.query(sql, [nombre,datosImagen], (err,result) => {
@@ -15,5 +15,22 @@ import sharp from "sharp";
         message:"Imagen agregada a bd"
     })
 }
+
+    export const publicarImagen = async (req,res) =>{
+        try{
+            const [imagen] = await connection.query('SELECT datosImagen FROM contenido_photo WHERE id = ?',[req.params.id])
+            console.log(imagen)
+            if(imagen.length <= 0) return res.status(404).json({
+                message:'Imagen not found'
+            })
+            res.contentType('image/jpeg');
+            res.end(imagen[0].datosImagen, 'binary');
+        }catch (error){
+            return res.status(500).json({
+                message:"Something went wrong"
+            })
+        }
+    }
+    
 
 export default {guardarImagen}
