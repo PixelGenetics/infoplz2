428f0694556e2b5fb3662f56a4dfae10e191d825
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-12-01 13:33:36 -0600

    Refactor to show image from endpoint

--- client/src/components/Content/Body.jsx
+++ client/src/components/Content/Body.jsx
@@ -1,5 +1,6 @@
 import { useEffect,useState } from "react"
 import axios from "axios"
+import MostrarImagen from "../MostrarImagen/MostrarImagen"
 const Body = () =>{
 
     const [inicio,setInicio] = useState([])
@@ -8,42 +9,24 @@ const Body = () =>{
         axios.get('http://localhost:3000/api/publicacion?page=1&limit=3').then(resp => {
             setInicio(resp.data.rows)
             console.log(resp.data.rows)
+            console.log("dasdsad",resp.data.rows[0].content_image)
         })
     },[])
 
-    const [imagenSrc,setImagenSrc] = useState(null)
-
-    useEffect(() => {
-        const obtenerImagen = async () => {
-          try {
-            const response = await axios.get(`http://localhost:3000/api/upload/1`, {
-              responseType: 'arraybuffer', // Indica que esperamos datos binarios en la respuesta
-            });
-    
-            const blob = new Blob([response.data], { type: 'image/jpeg' });
-            const imageUrl = URL.createObjectURL(blob);
-            setImagenSrc(imageUrl);
-          } catch (error) {
-            console.error('Error al obtener la imagen:', error);
-          }
-        };
-    
-        obtenerImagen();
-      }, []);
-    
 
     return(
         <>
         <div className="border-2 border-red-400 flex pl-[400px] pr-[400px]">
 
-
-        {imagenSrc && <img src={imagenSrc} alt="Imagen" />}  
         {
             inicio.map(item => (
             
                 <div key={item.content_id} className="border-2 border-blue-400 gap h-[20%]">
+                    {/* <MostrarImagen /> */}
                     <p className="mb-4">Author: {item.content_author}</p>
                     <p>{item.content_summary}</p>
+                    <img src={item.content_image} alt="" />
+                    {console.log("Bandera:",item.content_image)}
                 </div>
             ))
         }
