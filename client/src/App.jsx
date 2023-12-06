7dbd4b65fca1412e402f0beeffd2f402ae435750
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-12-05 22:10:05 -0600

    Creating path to error page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes,Route } from 'react-router-dom';
 import Home from './components/Home/Home.jsx';
 import UploadContent from './components/UploadContent/UploadContent.jsx';
+import Error404 from './404/Error404.jsx';
 function App() {
 
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/upload' element={<UploadContent />}></Route>
+          <Route path='/404' element={<Error404 />}></Route>
         </Routes>
       </h1>
 
