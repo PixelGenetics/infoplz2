976b931366cd4f3f2831e6ea54fd234202bf5bac
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-12-05 22:09:00 -0600

    Deleting route

--- server/routes/employes.routes.js
+++ server/routes/employes.routes.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import { deletePublicacion, getPublicaciones, postPublicacion, updatePublicacion, getPublicacion } 
 from "../controllers/publicar.controllers.js";
-import multer from "multer";
-import {guardarImagen, publicarImagen} from "../controllers/storage.controller.js";
+import { publicarImagen} from "../controllers/storage.controller.js";
 
 const router = Router();
 
@@ -11,13 +10,6 @@ router.get('/publicacion',getPublicaciones);
 router.get('/publicacion/:id', getPublicacion);
 
 router.post('/publicacion', postPublicacion);
-
-const storage = multer.memoryStorage();
-const subir = multer({storage:storage});
-
-router.post('/upload', subir.single('imagen'),(req,res) => {
-    guardarImagen(req,res);
-})
 
 router.get('/upload/:id',publicarImagen)
 
