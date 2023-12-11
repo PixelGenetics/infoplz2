0a4a74c733afda01ab44bf58429422944651e63f
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-12-10 21:02:05 -0600

    Adding img to 404

--- client/src/404/Error404.jsx
+++ client/src/404/Error404.jsx
@@ -1,11 +1,13 @@
+import imagen from "../assets/error404.jpg"
+
 const Error404 = () => {
+
+    
 
     return(
         <>
-
-        <div>
-        <p className="">dasdasdasdsadsdasdsadasds</p>
-            
+        <div className="flex justify-center w-[100%]">
+            <img src={imagen} alt="" className="w-[20%] mt-[15%]" />
         </div>
         </>
     )
