d647e8c12074b418cdba70b8033d788c52e546bb
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-12-18 17:30:29 -0600

    Creating routes to show all posts and limited posts

--- server/routes/employes.routes.js
+++ server/routes/employes.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deletePublicacion, getPublicaciones, postPublicacion, updatePublicacion, getPublicacion, getPublicacionesGeneral } 
+import { deletePublicacion, getPublicaciones, postPublicacion, updatePublicacion, getPublicacion, getPublicacionesGeneral, getPublicacionesReves, getPublicacionesRevesLimited } 
 from "../controllers/publicar.controllers.js";
 import { publicarImagen} from "../controllers/storage.controller.js";
 
@@ -9,7 +9,11 @@ router.get('/publicacion',getPublicaciones);
 
 router.get('/publicacion/:id', getPublicacion);
 
-router.get('/publicacionesgeneral',getPublicacionesGeneral)
+router.get('/publicacionesgeneral',getPublicacionesGeneral);
+
+router.get('/publicacionreveslimited',getPublicacionesRevesLimited);
+
+router.get('/publicacionreves',getPublicacionesReves)
 
 router.post('/publicacion', postPublicacion);
 
