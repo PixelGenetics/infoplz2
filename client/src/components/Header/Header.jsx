23a1284f6309936581f86454a24e41797aa9f2ac
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2024-01-11 19:19:06 -0600

    removing tailwind

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,16 +1,15 @@
 const Header = () =>{
     return(
     <>
-    <div className='  '>
-
     <div>
-        <ul className='flex flex-row gap-20 justify-center pt-5 pb-5'>
-            <button><li className='border-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-1 rounded-md shadow-xl'>Home</li></button>
-            <button><li className='border-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-1 rounded-md shadow-xl'>Articulos</li></button>
-            <button><li className='border-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% p-1 rounded-md shadow-xl'>Linkedin</li></button>
-        </ul>
-    </div>
-
+        <div>
+            <ul>
+                <li >Logo</li>
+                <button><li className=''>Home</li></button>
+                <button><li className=''>Articulos</li></button>
+                <button><li className=''>Linkedin</li></button>
+            </ul>
+        </div>
     </div>
     </>
     )
