3fe231e1361b729015efb91828a4341e3392966e
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2024-01-11 19:19:16 -0600

    Adding fonts

--- client/src/components/UploadContent/UploadContent.jsx
+++ client/src/components/UploadContent/UploadContent.jsx
@@ -1,45 +1,77 @@
 import axios from "axios"
-import { useEffect,useState } from "react"
 import {useForm} from 'react-hook-form'
 import { useNavigate } from "react-router-dom"
+import "../UploadContent/UploadContentStyle.css"
+import LoadFonts from "../Config/LoadFonts"
+import { useEffect,useState } from "react"
+
 
     // export async function uploadContent (content_author,
     // content_summary,content_parrafo,content_date,content_image)
     
     const UploadContent = () => {
 
+    const [fontsLoaded, setFontsLoaded] = useState(false);
     const {register, handleSubmit} = useForm()
+    const [inicio,setInicio] = useState([]);
 
     const navigate = useNavigate()
 
     const onSubmit = handleSubmit((data) => {
         console.log(data)
             axios.post('http://localhost:3000/api/publicacion',data).catch(error => {
+                console.log("error: ",error)
             navigate('/404')
         })
     })
 
     const paginaError = () => <p>Error404</p>
 
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/publicacionreveslimited').then(resp => {
+    console.log("submit", resp.data.rows)
+    setInicio(resp.data.rows)
+        });
+    }, []);
+    
+    useEffect(() => {
+        LoadFonts(() => {
+        setFontsLoaded(true);
+        })
+    })
+
+    const titleStyle = {
+        fontFamily: fontsLoaded ? 'Quicksand, sans-serif' : 'serif',
+        fontWeight: '600',
+        fontSize: '36px',
+        marginBottom:'16px'
+    };
+
+    const contentStyle = {
+        fontFamily: fontsLoaded ? 'Lato, sans-serif' : 'serif',
+        fontWeight: '300',
+        fontSize:'18px',
+        marginBottom:'16px'
+    };
     // let data = {onSubmit}
     // console.log("data",data)
 
     return(
             <>
-                <div className="flex justify-center">
-                    <form onSubmit={onSubmit} className="border-2 border-red-400">
-                        <div className="flex flex-col text-center gap-2 w-[500px]">
-                        <label htmlFor="">Autor</label>
-                        <input type="text" defaultValue="" {...register("content_author")} className="rounded-md "/>
-                        <label htmlFor="">Resumen</label>
+                <div className="UploadContent">
+                    <form onSubmit={onSubmit} className="">
+                        <div style={contentStyle} className="formContent">
+                        <label htmlFor="" style={titleStyle}>Autor</label>
+                        <input type="text" defaultValue="" {...register("content_author")}/>
+                        <label htmlFor="" style={titleStyle}>Resumen</label>
                         <textarea name="" id="" cols="30" rows="5" {...register("content_summary")}></textarea>
-                        <label htmlFor="">Informacion</label>
+                        <label htmlFor="" style={titleStyle}>Informacion</label>
                         <textarea name="" id="" cols="30" rows="10" {...register("content_parrafo")}></textarea>
-                        <label htmlFor="">Fecha</label>
-                        <input type="text" defaultValue="" {...register("content_date")} className="rounded-md"/>
-                        <label htmlFor="">Link de imagen</label>
-                        <input type="text" defaultValue="" {...register("content_image")} className="rounded-md"/>
-                        <button type="submit" className="border-2 border-blue-400 bg-slate-500">Subir</button>
+                        <label htmlFor="" style={titleStyle}>Fecha</label>
+                        <input type="text" defaultValue="" {...register("content_date")} className=""/>
+                        <label htmlFor="" style={titleStyle}>Link de imagen</label>
+                        <input type="text" defaultValue="" {...register("content_image")} className=""/>
+                        <button type="submit" className="">Subir</button>
                         </div>
                     </form>
                 </div>
