286e68104268290252e97cd08f43e5d20a6d8870
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2024-01-13 20:35:43 -0600

    Adding fonts to component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,52 @@
-const Header = () =>{
+import { useEffect,useState } from "react"
+import LoadFonts from "../Config/LoadFonts"
+import './Header.css'
+
+const Header = () =>{        
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+
+    useEffect(() => {
+        LoadFonts(() => {
+        setFontsLoaded(true);
+        })
+    })
+
+    const titleStyle = {
+        fontFamily: fontsLoaded ? 'Quicksand, sans-serif' : 'serif',
+        fontWeight: '500',
+        fontSize: '14px',
+    };
+
+    const contentStyle = {
+        fontFamily: fontsLoaded ? 'Lato, sans-serif' : 'serif',
+        fontWeight: '300',
+        fontSize:'18px',
+    };
+
     return(
+
     <>
-    <div>
-        <div>
+    <div className='header-component'>
+        <div className='inner-header-component'>
             <ul>
-                <li >Logo</li>
-                <button><li className=''>Home</li></button>
-                <button><li className=''>Articulos</li></button>
-                <button><li className=''>Linkedin</li></button>
+                <button style={titleStyle}><li><a href="">Logo</a></li></button>
+                <button style={titleStyle}><li><a href="">Explore</a></li></button>
+                <button style={titleStyle}><li><a href="">Software Dev</a></li></button>
+                <button style={titleStyle}><li><a href="">Cloud</a></li></button>
+                <button style={titleStyle}><li><a href="">IT Ops</a></li></button>
+                <button style={titleStyle}><li><a href="">Data</a></li></button>
+                <button style={titleStyle}><li><a href="">Security</a></li></button>
+                <button style={titleStyle}><li><a href="">Leadership</a></li></button>
             </ul>
         </div>
+        <div className='inner-header-component-2'>
+            <ul>
+                <button style={titleStyle}><li><a href="">Lupa</a></li></button>
+                <button style={titleStyle}><li><a href="">Contact Sales</a></li></button>
+                <button style={titleStyle}><li><a href="">View Plans</a></li></button>
+            </ul>
+        </div>
+
     </div>
     </>
     )
