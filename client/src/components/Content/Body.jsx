465bf6e17baa6b1ff1c4f4fd599713b0bdbcf0a4
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2024-01-15 01:10:24 -0600

    Creating search bar

--- client/src/components/Content/Body.jsx
+++ client/src/components/Content/Body.jsx
@@ -1,86 +1,96 @@
-import { useEffect,useState } from "react"
-import axios from "axios"
-import LoadFonts from "../Config/LoadFonts"
-import './Body.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import LoadFonts from "../Config/LoadFonts";
+import './Body.css';
 
-const Body = () =>{
-
-    const [inicio,setInicio] = useState([])
-    const [pagina,setPagina] = useState([])
+    const Body = () => {
+    const [results, setResults] = useState([]);
+    const [inicio, setInicio] = useState([]);
     const [fontsLoaded, setFontsLoaded] = useState(false);
+    const [search, setSearch] = useState("");
 
-    useEffect(() => {
-        axios.get('http://localhost:3000/api/publicacionreveslimited').then(resp => {
-            setInicio(resp.data.rows)
-        });
-    }, []);
-    
-    useEffect(() => {
-        LoadFonts(() => {
-        setFontsLoaded(true);
-        })
-    })
+    const loadData = async () => {
+        try {
+        const response = await axios.get('http://localhost:3000/api/publicacionreveslimited');
+        setResults(response.data.rows);
+        setInicio(response.data.rows);
+        } catch (error) {
+        console.error('Error al cargar los datos:', error);
+        }
+    };
+
     const authorStyle = {
         fontFamily: fontsLoaded ? 'Quicksand, sans-serif' : 'serif',
         fontWeight: '600',
         fontSize: '18px',
-        marginBottom:'16px'
+        marginBottom: '16px',
     };
     const titleStyle = {
         fontFamily: fontsLoaded ? 'Quicksand, sans-serif' : 'serif',
         fontWeight: '600',
         fontSize: '20px',
-        marginBottom:'16px'
+        marginBottom: '16px',
     };
 
     const contentStyle = {
         fontFamily: fontsLoaded ? 'Lato, sans-serif' : 'serif',
         fontWeight: '300',
-        fontSize:'18px',
-        marginBottom:'16px'
+        fontSize: '18px',
+        marginBottom: '16px',
     };
 
-    return(
+    useEffect(() => {
+        loadData();
+        LoadFonts(() => {
+        setFontsLoaded(true);
+        });
+    }, []);
+
+    const searcher = (event) => {
+        const searchTerm = event.target.value;
+        setSearch(searchTerm);
+
+        // Filtra los resultados basándonos en el término de búsqueda
+        const filteredResults = inicio.filter((item) =>
+        item.content_title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
+        setResults(filteredResults);
+    };
+
+    return (
         <>
-            <div className="explore-techonology">
-                <div className="inner-explore-tech">
-                    <h2 style={titleStyle}>Explore Technology </h2>
+        <div className="explore-techonology">
+            <div className="inner-explore-tech">
+            <input value={search} onChange={searcher} type="text" />
+            <h2 style={titleStyle}>Explore Technology </h2>
+            </div>
+        </div>
+        <div className="outter-div">
+            {results.map((item) => (
+            <div key={item.content_id} className="main-div">
+                <div className="inner-main-div">
+                <img src={item.content_image} alt="" className="inner-main-div-imagen" />
+                <h2 style={authorStyle} className="">
+                    {item.content_author}
+                </h2>
+                <h3 style={titleStyle}>{item.content_title}</h3>
+                <p style={contentStyle}>{item.content_summary}</p>
                 </div>
             </div>
-        <div className="outter-div">
-
-        {
-            inicio.map(item => (
-            
-                <div key={item.content_id} className="main-div">
-                    <div className="inner-main-div">
-                        <img src={item.content_image} alt="" className="inner-main-div-imagen"/>
-                        <h2 style={authorStyle} className="">{item.content_author}</h2>
-                        <h3 style={titleStyle}>{item.content_title}</h3>
-                        <p style={contentStyle}>{item.content_summary}</p>
-                    </div>
+            ))}
+            {results.map((item) => (
+            <div key={item.content_id} className="extra-blogs">
+                <div className="inner-extra-blogs">
+                <h2 style={authorStyle}>{item.content_author}</h2>
+                <h3 style={titleStyle}>{item.content_title}</h3>
+                <p style={contentStyle}>{item.content_summary}</p>
                 </div>
-
-
-            ))
-        }
-
-        {
-            inicio.map(item => (
-                <div key={item.content_id} className="extra-blogs">
-                    <div className="inner-extra-blogs">
-                        <h2 style={authorStyle}>{item.content_author}</h2>
-                        <h3 style={titleStyle}>{item.content_title}</h3>
-                        <p style={contentStyle}>{item.content_summary}</p>
-                    </div>
-                </div>
-            ))
-        }
-
-
+            </div>
+            ))}
         </div>
         </>
-    )
-}
+    );
+    };
 
-export default Body
+export default Body;
