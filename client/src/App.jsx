c44c4dfb2278a5fd4fb51969154f61ed1926a904
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2024-01-16 11:41:56 -0600

    Creating path search

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes,Route } from 'react-router-dom';
 import Home from './components/Home/Home.jsx';
 import UploadContent from './components/UploadContent/UploadContent.jsx';
 import Error404 from './404/Error404.jsx';
+import SearchComponent from './components/SearchComponent/SearchComponent.jsx';
 function App() {
 
 
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Home/>}></Route>
           <Route path='/upload' element={<UploadContent />}></Route>
           <Route path='/404' element={<Error404 />}></Route>
+          <Route path='/search' element={<SearchComponent />}></Route>
         </Routes>
       </h1>
     </div>
