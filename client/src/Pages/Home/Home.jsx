f146d83780cab6e62a68f0d0ea04464ebb557525
layeska <lisa13suarez@gmail.com>
2024-02-02 19:52:54 -0600

    Agregando footer

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from '../../components/Header/Header.jsx'
 import Body from '../../components/Content/Body.jsx'
+import Footer from '../../components/Footer/Footer.jsx'
 
 const Home = () =>{
     return(
         <>
             <Header />
             <Body/>
+            <Footer />
         </>
     )
 }
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Sitio desarrollado por <a href="#">Ana Juárez</a> & <a href="#">Kevin Borge</a></p>
+            <p>Todos los derechos reservados | 2024 </p>
+        </footer>
+    )
+}
+
+export default Footer
